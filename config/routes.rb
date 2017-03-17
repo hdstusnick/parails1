c6ab9247d1499e8911781541c8304249a1c9fb30
hdstusnick <hdstusnick@gmail.com>
2017-03-17 01:47:30 -0400

    This is not so bad after all; added signup need to work on login and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,12 @@ Rails.application.routes.draw do
   get  'static_pages/about'
   get 'static_pages/help'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'static_pages#home'
+  root 'users#new'
 
 end
